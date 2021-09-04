e1f6ba17b6bb87f4e43d19b11d7e6e54665576d8
Henrique Souza <souzahgg@gmail.com>
2021-09-05 11:42:47 +1200

    Better checking function

--- caesar.c
+++ caesar.c
@@ -17,7 +17,7 @@ int main(int argc, string argv[])
     }
     for (int i = 0; i < strlen(argv[1]); i++)
     {
-        if (isalpha(argv[1][i]))
+        if (!isdigit(argv[1][i]))
         {
             printf("Usage: ./caesar key\n");
             return 1;
